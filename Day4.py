37797c018d20cebfa6ef49abac66cfa1988ad711
SAURABH BISSA <64043023+SaurabhBissa@users.noreply.github.com>
2021-06-14 23:57:00 +0530

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,23 @@
+# Day 4:
+
+# 1) use string functions to count the occurrence of 'y' in a word given by user.
+
+# 2) take input of a string and print it's even indexed characters
+
+# 3)create a program to swap case. Using string functions
+# Input : EdUyEaR
+# Output :.  eDuYeAr
+
+
+# 1) count occurence
+str = input("Enter String : ")
+# print(dir(str))
+# print(str.count('y'))
+
+# 2) print even index
+print(str)
+print(str[1: :2])
+# 3) swap case
+# print("EdUyEaR")
+# print("EdUyEaR".swapcase())
+
